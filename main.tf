70568a72d98084235b40286afa9754352a705077
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-11-30 03:04:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ resource "aws_efs_file_system" "efsWordPress" {
 }
  
 data "aws_subnet_ids" "suballIDs" {
-    vpc_id = var.vpc_id
+    vpc_id = data.aws_vpc.suballIDs.id
 }
  
 resource "aws_efs_mount_target" "mtWordPress" {
